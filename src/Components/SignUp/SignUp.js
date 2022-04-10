2e73fef6350a05fc797cabfa38d3659c122b8962
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-10 07:21:16 +0600

    login validation

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,34 +1,50 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import GoogleLogo from "../../images/google.svg";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
+
 const SignUp = () => {
 
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    const [confirmPassword,setConfirmPassword]=useState('');
-    const [error,setError]=useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
 
-    const handleEmailBlur=(event) => {
+    const [createUserWithEmailAndPassword, hookError, user] = useCreateUserWithEmailAndPassword(auth);
+    const handleEmailBlur = (event) => {
         setEmail(event.target.value);
     }
-    const handlePasswordBlur=(event) => {
+    const handlePasswordBlur = (event) => {
         setPassword(event.target.value);
     }
-    const handleConfirmPasswordBlur=(event) => {
+    const handleConfirmPasswordBlur = (event) => {
         setConfirmPassword(event.target.value);
     }
-    const handleCreateUser=(event) => {
+
+    if (user) {
+        navigate('/shop');
+    }
+    const handleCreateUser = (event) => {
         event.preventDefault();
-        if (password!==confirmPassword) {
-            setError('your password not match')
+        if (password !== confirmPassword) {
+            setError('Your password not match')
             return;
-            
+
         }
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters long')
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password)
+
     }
     return (
         <div className="w-1/3 mx-auto border-2 shadow-md rounded-md bg-gray-300">
             <h2 className="text-center text-xl font-semibold p-4">SignUp</h2>
-            <div onSubmit={handleCreateUser} className="ml-32">
+            <form onSubmit={handleCreateUser} className="ml-32">
                 <div>
                     <h1 htmlFor="email">Email: </h1>
                     <input onBlur={handleEmailBlur} type="email" className="placeholder-shown:border-gray-500 border-2 text-center px-2 w-80 h-12 rounded-2" placeholder="Email" required />
@@ -37,25 +53,20 @@ const SignUp = () => {
 
                 <div className="mt-2">
                     <h1 htmlFor="email">Password: </h1>
-                    <input onBlur={handlePasswordBlur} type="email" className="placeholder-shown:border-gray-500 border-2 text-center px-2 w-80 h-12 rounded-2" placeholder="Password" required />
+                    <input onBlur={handlePasswordBlur} type="password" className="placeholder-shown:border-gray-500 border-2 text-center px-2 w-80 h-12 rounded-2" placeholder="Password" required />
                 </div>
 
                 <div className="mt-2">
                     <h1 htmlFor="email">Confirm Password: </h1>
-                    <input onBlur={handleConfirmPasswordBlur} type="email" className="placeholder-shown:border-gray-500 border-2 text-center px-2 w-80 h-12 rounded-2" placeholder="Confirm Password" required />
+                    <input onBlur={handleConfirmPasswordBlur} type="password" className="placeholder-shown:border-gray-500 border-2 text-center px-2 w-80 h-12 rounded-2" placeholder="Confirm Password" required />
                 </div>
 
-                    <p className="text-center text-xl text-red-600">{error}</p>
-                    
+                <p className="text-center text-xl text-red-600 mr-16 mt-3">{error}</p>
+                <p className="text-center text-xl text-red-600 mr-16 mt-3">{hookError}</p>
+
                 <button className="mx-auto w-80 h-12 border-3 mt-3 bg-purple-600 text-white font-semibold rounded-md">SignUp</button>
-            </div>
-            <p className="text-center p-3">Already Have an Account? <Link className="underline text-amber-500" to="/login">Login</Link></p>
-
-            {/* <div className='horizontal-divider'>
-
-                <p>or</p>
-                <div className='line-right' />
-            </div> */}
+            </form>
+            <p className="text-center p-3">Already Have an Account? <Link className="underline text-emerald-700" to="/login">Login</Link></p>
 
 
             <div className='ml-32  mb-3 rounded-md'>
